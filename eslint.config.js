687a64fbcd002cca1db5e82155751bb613f543d8
Uladzislau <u.shylko25@gmail.com>
2026-01-12 20:56:15 +0300

    init: add eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,17 @@ import globals from "globals"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
-import { defineConfig, globalIgnores } from "eslint/config"
+import { defineConfig } from "eslint/config"
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+export default defineConfig(
+  {
+    ignores: ["dist"],
+  },
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -23,4 +25,4 @@ export default defineConfig([
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
-])
+)
