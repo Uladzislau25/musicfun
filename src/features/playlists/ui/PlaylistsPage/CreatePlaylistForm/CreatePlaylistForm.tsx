e5041a0514c6f9453324ed5795f25f65f42e0e35
Uladzislau <u.shylko25@gmail.com>
2026-01-13 19:25:27 +0300

    feat: add CreatePlaylistForm

---
+++ src/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx
@@ -0,0 +1,26 @@
+import { type SubmitHandler, useForm } from "react-hook-form"
+import type { CreatePlaylistArgs } from "@/features/playlists/api/playlistsApi.types.ts"
+import { useCreatePlaylistMutation } from "@/features/playlists/api/playlistApi.ts"
+
+export const CreatePlaylistForm = () => {
+  const { register, handleSubmit } = useForm<CreatePlaylistArgs>()
+
+  const [createPlaylist] = useCreatePlaylistMutation()
+
+  const onSubmit: SubmitHandler<CreatePlaylistArgs> = (data) => {
+    createPlaylist(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>Create new playlist</h2>
+      <div>
+        <input {...register("title")} placeholder={"title"} />
+      </div>
+      <div>
+        <input {...register("description")} placeholder={"description"} />
+      </div>
+      <button>create playlist</button>
+    </form>
+  )
+}
--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,5 +1,6 @@
 import { useFetchPlaylistsQuery } from "@/features/playlists/api/playlistApi.ts"
 import s from "./PlaylistsPage.module.css"
+import { CreatePlaylistForm } from "@/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx"
 
 export const PlaylistsPage = () => {
   const { data } = useFetchPlaylistsQuery()
@@ -7,6 +8,7 @@ export const PlaylistsPage = () => {
   return (
     <div className={s.container}>
       <h1>Playlists page</h1>
+      <CreatePlaylistForm />
       <div className={s.items}>
         {data?.data.map((playlist) => {
           return (
