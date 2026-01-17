81def9818c64080eb9bb6065e64387a111c8e741
Uladzislau <u.shylko25@gmail.com>
2026-01-17 22:12:32 +0300

    feat: add images

--- src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx
@@ -1,5 +1,6 @@
 import type { PlaylistData } from "@/features/playlists/api/playlistsApi.types.ts"
-
+import defaultCover from "@/assets/images/default-playlist-cover.png"
+import s from "./PlaylistItem.module.css"
 type Props = {
   playlist: PlaylistData
   deletePlaylistHandler: (playlistId: string) => void
@@ -8,6 +9,7 @@ type Props = {
 export const PlaylistItem = ({ playlist, editPlaylistHandler, deletePlaylistHandler }: Props) => {
   return (
     <div>
+      <img src={defaultCover} alt="cover" width={"100px"} className={s.cover} />
       <div>title: {playlist.attributes.title}</div>
       <div>description: {playlist.attributes.description}</div>
       <div>userName: {playlist.attributes.user.name}</div>
