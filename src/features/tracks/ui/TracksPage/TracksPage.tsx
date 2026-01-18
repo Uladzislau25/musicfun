5139ec22f7153be6c281d6991e53ef52d4832785
Uladzislau <u.shylko25@gmail.com>
2026-01-18 14:51:18 +0300

    feat: add infinity scroll

--- src/features/tracks/ui/TracksPage/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage/TracksPage.tsx
@@ -1,16 +1,44 @@
 import { useFetchTracksInfiniteQuery } from "@/features/tracks/api/tracksApi.ts"
 import s from "./TracksPage.module.css"
+import { useCallback, useEffect, useRef } from "react"
 
 export const TracksPage = () => {
-  const { data, hasNextPage, isLoading, isFetching, isFetchingNextPage, fetchNextPage } = useFetchTracksInfiniteQuery()
-  console.log(data)
+  const { data, hasNextPage, isFetching, isFetchingNextPage, fetchNextPage } = useFetchTracksInfiniteQuery()
+
+  const observerRef = useRef<HTMLDivElement>(null)
+
   const pages = data?.pages.flatMap((page) => page.data) || []
 
-  const loadMoreHandler = () => {
+  const loadMoreHandler = useCallback(() => {
     if (hasNextPage && !isFetching) {
       fetchNextPage()
     }
-  }
+  }, [hasNextPage, isFetching, fetchNextPage])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries.length > 0 && entries[0].isIntersecting) {
+          loadMoreHandler()
+        }
+      },
+      {
+        root: null,
+        rootMargin: "100px",
+        threshold: 0.1,
+      },
+    )
+
+    const currentObserverRef = observerRef.current
+    if (currentObserverRef) {
+      observer.observe(currentObserverRef)
+    }
+    return () => {
+      if (currentObserverRef) {
+        observer.unobserve(currentObserverRef)
+      }
+    }
+  }, [loadMoreHandler])
 
   return (
     <div>
@@ -29,17 +57,12 @@ export const TracksPage = () => {
           )
         })}
       </div>
-      {!isLoading && (
-        <>
-          {hasNextPage ? (
-            <button onClick={loadMoreHandler} disabled={isFetching}>
-              {isFetchingNextPage ? "Loading..." : "Load more"}
-            </button>
-          ) : (
-            <p>Nothing more to load</p>
-          )}
-        </>
+      {hasNextPage && (
+        <div ref={observerRef}>
+          {isFetchingNextPage ? <div>Loading more tracks...</div> : <div style={{ height: "20px" }}></div>}
+        </div>
       )}
+      {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p>}
     </div>
   )
 }
