7e9f2f9c07527099add5cc46f6cff98225648238
Uladzislau <u.shylko25@gmail.com>
2026-01-25 19:51:28 +0300

    feat: delete isErrorWithError and isErrorWithMessage

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,4 +1,2 @@
 export { getPaginationPages } from "./getPaginationPages.ts"
-export { isErrorWithMessage } from "./isErrorWithMessage.ts"
-export { isErrorWithError } from "./isErrorWithError.ts"
 export { isErrorWithProperty } from "./isErrorWithProperty.ts"
--- src/common/utils/isErrorWithProperty.ts
+++ src/common/utils/isErrorWithProperty.ts
@@ -1,10 +1,10 @@
-/* Синтаксис error is { error: string } означает, что если функция возвращает true, TypeScript будет рассматривать
-error как объект с обязательным строковым свойством message. */
+/* Синтаксис error is { T: string } означает, что если функция возвращает true, TypeScript будет рассматривать
+error как объект с обязательным строковым свойством T. */
 export function isErrorWithProperty<T extends string>(error: unknown, property: T): error is Record<T, string> {
   return (
     typeof error === "object" && // Проверяем, что error – это объект
     error != null && // Убеждаемся, что это не null
-    property in error && // Проверяем, что у объекта есть свойство 'error'
+    property in error && // Проверяем, что у объекта есть свойство 'T'
     typeof (error as Record<string, unknown>)[property] === "string" // Убеждаемся, что это строка
   )
 }
