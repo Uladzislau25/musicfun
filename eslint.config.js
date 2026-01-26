d8302c53fbb6f3ff1b7d8d07300ee0cc2327b3b4
Uladzislau <u.shylko25@gmail.com>
2026-01-26 20:12:01 +0300

    feat: add handleErrors

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
+import unusedImports from "eslint-plugin-unused-imports"
 import { defineConfig } from "eslint/config"
 
 export default defineConfig(
@@ -17,11 +18,23 @@ export default defineConfig(
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      "unused-imports": unusedImports,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-unused-imports": "off",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
