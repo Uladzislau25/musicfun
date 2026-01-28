540f7af9e0d06a180f33f625976e21dc83d70182
Uladzislau <u.shylko25@gmail.com>
2026-01-28 18:55:54 +0300

    feat: add MessageEvent

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,16 +1,23 @@
 import { useLoginMutation } from "@/features/auth/api/authApi.ts"
+import { Path } from "@/common/routing"
 
 export const Login = () => {
   const [login] = useLoginMutation()
   const loginHandler = () => {
-    const redirectUri = import.meta.env.VITE_DOMAIN_ADDRESS + "/oauth/callback"
+    const redirectUri = import.meta.env.VITE_DOMAIN_ADDRESS + Path.OAuthRedirect
     const url = `${import.meta.env.VITE_BASE_URL}/auth/oauth-redirect?callbackURL=${redirectUri}`
 
     window.open(url, "oauthPopop", "width=500, height=600")
     const receiveMessage = (event: MessageEvent) => {
+      if (event.origin !== import.meta.env.VITE_DOMAIN_ADDRESS) return
+
+      const { code } = event.data
+
+      if (!code) return
+
       window.removeEventListener("message", receiveMessage)
       login({
-        code: "",
+        code,
         redirectUri,
         rememberMe: false,
       })
