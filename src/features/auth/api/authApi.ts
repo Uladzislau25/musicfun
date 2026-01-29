0f16d97170382d8d7fe5c1d49d2b45992da31512
Uladzislau <u.shylko25@gmail.com>
2026-01-29 19:11:31 +0300

    feat: add dispatch reset in authApi

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -32,10 +32,11 @@ export const authApi = baseApi.injectEndpoints({
           body: { refreshToken },
         }
       },
-      onQueryStarted: async (_args, { queryFulfilled }) => {
+      onQueryStarted: async (_args, { dispatch, queryFulfilled }) => {
         await queryFulfilled
         localStorage.removeItem(AUTH_KEYS.accessToken)
         localStorage.removeItem(AUTH_KEYS.refreshToken)
+        dispatch(baseApi.util.resetApiState())
       },
     }),
   }),
