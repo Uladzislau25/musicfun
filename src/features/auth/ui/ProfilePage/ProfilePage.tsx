0a71e239a3425706a04910948a71775771aa5a8a
Uladzislau <u.shylko25@gmail.com>
2026-01-30 22:48:53 +0300

    feat: add playlists in ProfilePage

--- src/features/auth/ui/ProfilePage/ProfilePage.tsx
+++ src/features/auth/ui/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,16 @@
 import { useGetMeQuery } from "@/features/auth/api/authApi.ts"
+import { useFetchPlaylistsQuery } from "@/features/playlists/api/playlistApi.ts"
+import { PlaylistList } from "@/features/playlists/ui/PlaylistsPage/PlaylistList/PlaylistList.tsx"
 
 export const ProfilePage = () => {
-  const { data } = useGetMeQuery()
-
-  return <h1>{data?.login} page</h1>
+  const { data: meResponse } = useGetMeQuery()
+  const { data: playlistsResponse, isLoading } = useFetchPlaylistsQuery({
+    userId: meResponse?.userId,
+  })
+  return (
+    <div>
+      <h1>{meResponse?.login} page</h1>
+      <PlaylistList isPlaylistLoading={isLoading} playlists={playlistsResponse?.data || []} />
+    </div>
+  )
 }
