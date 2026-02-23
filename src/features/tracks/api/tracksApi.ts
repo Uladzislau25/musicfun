3c5d41905984b894b0f5300f0f6ada127ac4373b
Uladzislau <u.shylko25@gmail.com>
2026-02-23 19:14:12 +0300

    feat: add withZodCath and schemas in tracksApi

--- src/features/tracks/api/tracksApi.ts
+++ src/features/tracks/api/tracksApi.ts
@@ -1,5 +1,7 @@
 import { baseApi } from "@/app/api/baseApi.ts"
 import type { FetchTracksResponse } from "@/features/tracks/api/tracksApi.types.ts"
+import { withZodCath } from "@/common/utils/withZodCath.ts"
+import { fetchTracksResponseSchema } from "@/features/tracks/model/tracks.schemas.ts"
 
 export const tracksApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
@@ -16,6 +18,7 @@ export const tracksApi = baseApi.injectEndpoints({
           params: { cursor: pageParam, pageSize: 5, paginationType: "cursor" },
         }
       },
+      ...withZodCath(fetchTracksResponseSchema),
     }),
   }),
 })
