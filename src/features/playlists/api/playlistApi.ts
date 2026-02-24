a0e93a524164f93d6fffa45b517219f8d1951a22
Uladzislau <u.shylko25@gmail.com>
2026-02-24 20:22:23 +0300

    feat: add subscribeToEvent in playlistApi

--- src/features/playlists/api/playlistApi.ts
+++ src/features/playlists/api/playlistApi.ts
@@ -10,7 +10,8 @@ import type { Images } from "@/common/types"
 import { playlistCreateResponseSchema, playlistResponseSchema } from "@/features/playlists/model/playlists.schemas.ts"
 import { imagesSchema } from "@/common/schemas/schemas.ts"
 import { withZodCath } from "@/common/utils/withZodCath.ts"
-import { io, Socket } from "socket.io-client"
+import { subscribeToEvent } from "@/common/socket/subscribeToEvent.ts"
+import { SOCKET_EVENTS } from "@/common/constants"
 
 export const playlistApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
@@ -22,29 +23,24 @@ export const playlistApi = baseApi.injectEndpoints({
         // Ждем разрешения начального запроса перед продолжением
         await cacheDataLoaded
 
-        // Создаем Socket.IO соединение с сервером
 
-        const socket: Socket = io("https://musicfun.it-incubator.app", {
-          path: "/api/1.0/ws", // пользовательский путь для Socket.IO сервера (по умолчанию '/socket.io/')
-          transports: ["websocket"],
-        })
-        socket.on("connect", () => console.log("Подключен к серверу"))
-        socket.on("tracks.playlist-created", (msg: PlaylistCreatedEvent) => {
-          const newPlaylist = msg.payload.data
-          updateCachedData((state) => {
-            state.data.pop()
-            state.data.unshift(newPlaylist)
-            state.meta.totalCount = state.meta.totalCount + 1
-            state.meta.pagesCount = Math.ceil(state.meta.totalCount / state.meta.pageSize)
-          })
-          // 2 вариант
-          // dispatch(playlistApi.util.invalidateTags(['Playlist'}))
-        })
+
+        const unsubscribe = subscribeToEvent<PlaylistCreatedEvent>(
+          SOCKET_EVENTS.PLAYLIST_CREATED,
+          msg => {
+            const newPlaylist = msg.payload.data
+            updateCachedData(state => {
+              state.data.pop()
+              state.data.unshift(newPlaylist)
+              state.meta.totalCount = state.meta.totalCount + 1
+              state.meta.pagesCount = Math.ceil(state.meta.pagesCount / state.meta.pageSize)
+            })
+          }
+        )
 
         // CacheEntryRemoved разрешится, когда подписка на кеш больше не активна
         await cacheEntryRemoved
-        // Выполняем шаги очистки после разрешения промиса 'cacheEntryRemoved'
-        socket.on("disconnect", () => console.log("Соединение разорвано"))
+        unsubscribe()
       },
       providesTags: ["Playlist"],
     }),
