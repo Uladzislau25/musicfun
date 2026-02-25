8e68901a2545711478494dff6d00260fff63253e
Uladzislau <u.shylko25@gmail.com>
2026-02-25 19:28:39 +0300

    feat: add array unsubscribes

--- src/features/playlists/api/playlistApi.ts
+++ src/features/playlists/api/playlistApi.ts
@@ -26,24 +26,24 @@ export const playlistApi = baseApi.injectEndpoints({
 
         const unsubscribes = [
           subscribeToEvent<PlaylistCreatedEvent>(SOCKET_EVENTS.PLAYLIST_CREATED, (msg) => {
-          const newPlaylist = msg.payload.data
-          updateCachedData((state) => {
-            state.data.pop()
-            state.data.unshift(newPlaylist)
-            state.meta.totalCount = state.meta.totalCount + 1
-            state.meta.pagesCount = Math.ceil(state.meta.pagesCount / state.meta.pageSize)
-          })
-        }),
-        subscribeToEvent<PlaylistUpdatedEvent>(SOCKET_EVENTS.PLAYLIST_UPDATED, (msg) => {
-          const newPlaylist = msg.payload.data
-          updateCachedData((state) => {
-            const index = state.data.findIndex((playlist) => playlist.id === newPlaylist.id)
-            if (index !== -1) {
-              state.data[index] = { ...state.data[index], ...newPlaylist }
-            }
-          })
-        })
-      ]
+            const newPlaylist = msg.payload.data
+            updateCachedData((state) => {
+              state.data.pop()
+              state.data.unshift(newPlaylist)
+              state.meta.totalCount = state.meta.totalCount + 1
+              state.meta.pagesCount = Math.ceil(state.meta.pagesCount / state.meta.pageSize)
+            })
+          }),
+          subscribeToEvent<PlaylistUpdatedEvent>(SOCKET_EVENTS.PLAYLIST_UPDATED, (msg) => {
+            const newPlaylist = msg.payload.data
+            updateCachedData((state) => {
+              const index = state.data.findIndex((playlist) => playlist.id === newPlaylist.id)
+              if (index !== -1) {
+                state.data[index] = { ...state.data[index], ...newPlaylist }
+              }
+            })
+          }),
+        ]
         // CacheEntryRemoved разрешится, когда подписка на кеш больше не активна
         await cacheEntryRemoved
         unsubscribes.forEach((unsubscribe) => unsubscribe())
